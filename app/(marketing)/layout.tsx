f8deacf1fbcbafb6c97eeda7c1a5d1e48a974e1c
FindMalek <malekgarahellalbus@gmail.com>
2024-02-09 15:58:05 +0100

    :bookmark: Version - 0.1.0 v

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -49,13 +49,21 @@ export const metadata = {
     title: siteConfig.name,
     description: siteConfig.description,
     siteName: siteConfig.name,
+    images: [
+      {
+        url: siteConfig.ogImage,
+        width: 1200,
+        height: 630,
+        alt: siteConfig.name,
+      },
+    ],
   },
   twitter: {
     card: "summary_large_image",
     title: siteConfig.name,
     description: siteConfig.description,
     images: [`${siteConfig.url}/og.jpg`],
-    creator: "@shadcn",
+    creator: "@foundmalek",
   },
   icons: {
     icon: "/favicon.ico",
