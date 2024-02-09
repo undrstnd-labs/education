f7dcf1d879bc776e5cdaa7a06f508ec8e5870a83
FindMalek <malekgarahellalbus@gmail.com>
2024-02-09 15:51:58 +0100

    :rocket: Prepared Development Enviroment.

--- config/site.ts
+++ config/site.ts
@@ -5,7 +5,7 @@ export const siteConfig: SiteConfig = {
   description:
     "L'avenir de l'apprentissage en ligne, rendu facile et interactif grâce à l'intelligence artificielle.",
   url: "https://undrstnd.vercel.app/",
-  ogImage: "https://undrstnd.vercel.app/og.png",
+  ogImage: "https://undrstnd.vercel.app/og2.png",
   links: {
     twitter: "https://twitter.com/foundmalek",
     github: "https://github.com/FindMalek/undrstnd",
