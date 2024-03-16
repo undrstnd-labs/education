f6de1f995cfecfccd53d0c953d986a81010503ab
FindMalek <malekgarahellalbus@gmail.com>
2024-03-16 19:56:15 +0100

    :bricks: Updated: Dashboard Pages & Layout. | Classroom

---
+++ app/[locale]/(dashboard)/dashboard/classroom/c/[chatId]/page.tsx
@@ -0,0 +1,14 @@
+// TODO: Generate metadata for this page
+
+export default function ChatPage({
+    params,
+}: {
+    params: { chatId: string };
+}) {
+    return (
+        <div>
+            <h1>Chat {params.chatId}</h1>
+        </div>
+    );
+}
+    
