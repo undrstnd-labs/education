f371b297f616a1db9bd7a32c78bb3db4793385de
Amine Jguirim <aminejguirim10@gmail.com>
2024-03-24 00:25:01 +0100

    :globe_with_meridians: Feature: Added Internationalization for Metadata

--- app/[locale]/(dashboard)/dashboard/page.tsx
+++ app/[locale]/(dashboard)/dashboard/page.tsx
@@ -1,9 +1,11 @@
 import { getCurrentUser } from "@lib/session";
+import { getTranslations } from "next-intl/server";
 
 // TODO: Generate metadata from the translation file
-export const metadata = {
-  title: "Dashboard",
-};
+export async function generateMetadata() {
+  const t = await getTranslations("Metadata.Pages.Dashboard");
+  return { title: `${t("title")}` };
+}
 
 export default async function DashboardPage() {
   const user = await getCurrentUser();
